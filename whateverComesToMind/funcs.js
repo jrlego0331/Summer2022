3c12e2d717cf20c52c89fb9e0a7d7db6ca404064
Ro <jrlego0331@gmail.com>
2022-08-19 00:37:11 -0400

    rather than multitasking and not finishing anything, I finished empty module js and figuring out better file structure

---
+++ whateverComesToMind/funcs.js
@@ -0,0 +1,25 @@
+function color(){
+    this.rgb = function(red, green, blue){return 'rgb('+red+','+green,','+blue+')';}
+    this.rgba = function(red, green, blue, attenuation=1.0){return 'rgb('+red+','+green+','+blue+','+attenuation+')';}
+    this.text = function(text){return text;}
+}
+
+function vec(vector){
+    this.magnitude=function(){return ((vector.x**2)+(vector.y**2))**0.5;}
+    this.direction=function(){return Math.atan(vector.y/vector.x);}
+}
+
+function degToRad(deg){return (2*Math.PI)*(deg/360);}
+function radToDeg(rad){return 360*(rad/(2*Math.PI));}
+
+function draw(canvas, ctx){
+    this.blank=function(col, lineWidth){
+        ctx.beginPath();
+        ctx.arc(0.5*canvas.width, 0.5*canvas.height, 0.25*canvas.height, 0*Math.PI, 2*Math.PI, true);
+        ctx.moveTo(0.5*canvas.width-0.3*canvas.height, 0.5*canvas.height-0.3*canvas.height);
+        ctx.lineTo(0.5*canvas.width+0.3*canvas.height, 0.5*canvas.height+0.3*canvas.height);
+        ctx.strokeStyle = col;
+        ctx.lineWidth = lineWidth;
+        ctx.stroke();
+    }
+}
